22e271977dcb30394f3afa15c470e8896a84b8fc
Selinofant <125209672+Selinofant@users.noreply.github.com>
2023-02-12 18:32:33 +0100

    Create feedback.py
    slash-Command der von allen auf dem Server genutzt werden kann. 
    Der User wählt aus, ob er "Feedback" geben oder einen "Vorschlag"/"Feedback" einreichen möchte. 
    Der User bekommt jeweils ein Modal ausgegeben, dass er ausfüllen kann. 
    benötigte Angaben: feedback-channel, vorschlag-forum (jeweils die ID)
    feedback-channel: Hier werden Infos zu den Vorschlägen/Beschwerden/feedback in verschiedenen Farben gesendet - sollte nur für Admin/Moderatoren zugänglich sein
    Vorschlag-forum: hier postet der Bot die Vorschläge zur Abstimmung, sollte für alle zugänglich sein und User sollten abstimmen dürfen.
    
    Feedback: 
    Das Modal fragt nach einem Titel sowie einer Beschreibung des Feedbacks. 
    Das Feedback wird in den feedback-channel als embed gesendet. (farbe:grün)
    
    Vorschlag:
    Das Modal fragt nach dem Titel sowie der Beschreibung des Vorschlags.
    Der Vorschlag wird ohne Nennung des Users in das dafür bestimmte Forum als neuer Post gesendet. 
    Der Bot reagiert auf den Post mit thumps up und thumps down - andere user können abstimmen. 
    Eine Info von wem er Vorschlag kommt wird in den feedback-channel gesendet als embed. (farbe:gelb)
    
    Beschwerde:
    Das Modal fragt nach dem Titel, Beschreibung und dem Wunsch, wie mit der Beschwerde umgegangen werden soll.
    Die Beschwerde wird vom Bot im feedback-channel als Embed geesendet. (farbe:rot)

---
+++ feedback.py
@@ -0,0 +1,154 @@
+import discord
+from discord.ext import commands
+from discord.commands import slash_command
+from discord.commands import Option
+import json
+
+with open("Channel.json") as channel:
+    channel = json.load(channel)
+
+feedback = channel["feedback"] #hier kommt die channel-ID des channels hin, in den Feedback/Beschwerden/Vorschläge gesendet werdn sollen
+voting = channel["vorschläge"] #hier kommt die forums-ID hin, wo die Vorschläge gepostet werden sollen
+
+
+class Feedback(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command(description="Gib Feedback, reiche einen Vorschlag/Beschwerde ein")
+    async def feedback(self, ctx, art:Option(str, choices=["Feedback", "Vorschlag", "Beschwerde"])):
+
+        if art == "Feedback":
+            modal = FeedbackModal(title="Teile dein Feedback")
+            await ctx.send_modal(modal)
+
+        if art == "Vorschlag":
+            modal = VorschlagModal(title="Reiche einen Vorschlag ein")
+            await ctx.send_modal(modal)
+
+        if art == "Beschwerde":
+            modal = BeschwerdeModal(title="Reiche eine Beeschwerde ein")
+            await ctx.send_modal(modal)
+
+
+class FeedbackModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            discord.ui.InputText(
+                label="Titel deines Feedbacks",
+                placeholder="Schreibe etwas...",
+                max_length=50
+            ),
+            discord.ui.InputText(
+                label="Feedback",
+                placeholder="Schreibe etwas...",
+                style=discord.InputTextStyle.long,
+                max_length=1000
+            ),
+            *args,
+            **kwargs
+        )
+
+    async def callback(self, interaction):
+        embed = discord.Embed(
+            title="Feedback",
+            description=self.children[0].value,
+            color=discord.Color.from_rgb(56, 165, 125)
+        )
+        Autor = f"Name: {interaction.user.name},  <@{interaction.user.id}>"
+        embed.add_field(name="Feedback von: ", value=Autor, inline=False)
+        embed.add_field(name="Beschreibung:", value=self.children[1].value, inline=False)
+
+        channel = interaction.guild.get_channel(feedback)
+        await interaction.response.send_message("Alles klar, wir haben dein Feedback erhalten", ephemeral=True)
+        await channel.send(embed=embed)
+
+
+class VorschlagModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            discord.ui.InputText(
+                label="Titel deines Vorschlags",
+                placeholder="Schreibe etwas...",
+                max_length=50
+
+            ),
+            discord.ui.InputText(
+                label="Beschreibung",
+                placeholder="Schreibe etwas...",
+                style=discord.InputTextStyle.long,
+                max_length=1000
+            ),
+            *args,
+            **kwargs
+        )
+
+    async def callback(self, interaction):
+        embed = discord.Embed(
+            title="Vorschlag",
+            description=self.children[0].value,
+            color=discord.Color.from_rgb(255, 255, 153)
+        )
+
+        Autor = f"Name: {interaction.user.name},  <@{interaction.user.id}>"
+        embed.add_field(name="Vorschlag von: ", value=Autor, inline=False)
+        embed.add_field(name="Beschreibung:", value=self.children[1].value, inline=False)
+
+        vorschlag = interaction.guild.get_channel(voting)
+        vote = await vorschlag.create_thread(
+            name=self.children[0].value,
+            content=self.children[1].value
+        )
+        await vote.starting_message.add_reaction(emoji='👍')
+        await vote.starting_message.add_reaction(emoji='👎')
+
+        channel = interaction.guild.get_channel(feedback)
+        await channel.send(embed=embed)
+        await interaction.response.send_message("Alles klar, wir haben deinen Vorschlag erhalten", ephemeral=True)
+
+
+class BeschwerdeModal(discord.ui.Modal):
+    def __init__(self, *args, **kwargs):
+        super().__init__(
+            discord.ui.InputText(
+                label="Titel deiner Beschwerde",
+                placeholder="Worum geht es?",
+                max_length=50
+            ),
+            discord.ui.InputText(
+                label="Beschreibung",
+                placeholder="Schreibe etwas...",
+                style=discord.InputTextStyle.long,
+                max_length=800
+
+            ),
+            discord.ui.InputText(
+                label="Wie geht es weiter?",
+                placeholder="Was wünscht du dir? Wie können wir das Problem gemeinsam lösen?",
+                style=discord.InputTextStyle.long,
+                max_length=800
+            ),
+            *args,
+            **kwargs
+        )
+
+    async def callback(self, interaction):
+        embed = discord.Embed(
+            title="Beschwerde",
+            description=self.children[0].value,
+            color=discord.Color.from_rgb(204, 0, 0)
+        )
+
+        Autor = f"Name: {interaction.user.name},  <@{interaction.user.id}>"
+        embed.add_field(name="Beschwerde von: ", value=Autor, inline=False)
+        embed.add_field(name="Beschreibung:", value=self.children[1].value, inline=False)
+        embed.add_field(name="Wunsch, wie das Problem gelöst wird: ", value=self.children[2].value, inline=False)
+
+        channel = interaction.guild.get_channel(feedback)
+        await interaction.response.send_message("Alles klar, wir haben deine Beschwerde erhalten", ephemeral=True)
+        await channel.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Feedback(bot))
+
